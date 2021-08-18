dad096d882d9cf96e712fdadce40b47683ae24db
JakubGala <snuq321@gmail.com>
2021-08-18 22:58:42 +0200

    added search form

--- JSON-Server/js/index.js
+++ JSON-Server/js/index.js
@@ -1,31 +1,38 @@
 // js for index.html
 
-const container = document.querySelector('.blogs');
+const container = document.querySelector(".blogs");
+const searchFrom = document.querySelector(".search");
+// term szuka
+const renderPosts = async (term) => {
+//   let uri = "http://localhost:3000/posts";
 
-const renderPosts = async () => {
-    let uri = 'http://localhost:3000/posts';
+  // sorted
+  let uri = 'http://localhost:3000/posts?_sort=likes$_order=desc';
+  if (term) {
+      uri += `&q=${term}`;
+  }
 
-    // sorted 
-    // let uri = 'http://localhost:3000/posts?_sort=likes$_order=desc';
+  const res = await fetch(uri);
+  const posts = await res.json();
 
-    const res = await fetch(uri);
-    const posts = await res.json();
-    
-
-    let template = '';
-    posts.forEach(post => {
-        template += `
+  let template = "";
+  posts.forEach((post) => {
+    template += `
         <div class="post">
            <h2>${post.title}</h2> 
            <p>${post.likes} likes</p>
-           <p>${post.body.slice(0 , 200)}</p>
+           <p>${post.body.slice(0, 200)}</p>
            <a href="/JSON-Server/details.html?id=${post.id}">read more...</a>
         </div>
-        `  
-    })
+        `;
+  });
 
-    container.innerHTML = template;
+  container.innerHTML = template;
+};
 
-}
+searchFrom.addEventListener('submit' , (e) => {
+    e.preventDefault();
+    renderPosts(searchFrom.term.value.trim())
+})
 
-window.addEventListener('DOMContentLoaded', () => renderPosts());
+window.addEventListener("DOMContentLoaded", () => renderPosts());
