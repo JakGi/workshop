d0921bfe6ae21b85d629a4a489e6cf81cae5802b
JakubGala <snuq321@gmail.com>
2021-08-18 22:48:31 +0200

    json deleting items

--- JSON-Server/js/details.js
+++ JSON-Server/js/details.js
@@ -3,7 +3,9 @@
 // (window.location.search) === ?id=1 2 3
 const id = new URLSearchParams(window.location.search).get("id");
 
-const container = document.querySelector('.details');
+const container = document.querySelector(".details");
+
+const deleteBtn = document.querySelector(".button");
 
 const renderDeatails = async () => {
   const res = await fetch("http://localhost:3000/posts/" + id);
@@ -14,7 +16,14 @@ const renderDeatails = async () => {
     <p>${post.body}</p>
     `;
 
-    container.innerHTML = template;
+  container.innerHTML = template;
 };
+
+deleteBtn.addEventListener("click", async (e) => {
+  const res = await fetch("http://localhost:3000/posts/" + id, {
+    method: "DELETE",
+  });
+  window.location.replace("/JSON-Server/index.html");
+});
 
 window.addEventListener("DOMContentLoaded", () => renderDeatails());
