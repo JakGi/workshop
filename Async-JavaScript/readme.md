885c3e35608641e257e9bd3b943af3ef5624847b
JakubGala <snuq321@gmail.com>
2021-08-19 10:13:11 +0200

    status codes

--- Async-JavaScript/readme.md
+++ Async-JavaScript/readme.md
@@ -1,2 +1,8 @@
-<h1>JSON server testing machine</h1>
+<h3>JSON server testing machine</h3>
 <a href="https://jsonplaceholder.typicode.com/">jsonplaceholder.typicode.com</a>
+<h3>status codes</h3>
+<a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status">Status codes</a>
+<h3>Ready state</h3>
+<a href="// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState">Ready state</a>
+
+
--- Async-JavaScript/sandbox.js
+++ Async-JavaScript/sandbox.js
@@ -4,12 +4,14 @@ const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
     // console.log(request, request.readyState);
-    if(request.readyState === 4) {
-        console.log(request.responseText)
+    if(request.readyState === 4 && request.status === 200) {
+        console.log(request, request.responseText)
+    }else if(request.readyState === 4){
+        console.log('could not fatch data')
     }
 });
 
 // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
 request.send();
