f0f4c7d64a5396994cbcf54def3c8a3bd11df381
JakubGala <snuq321@gmail.com>
2021-10-05 13:24:19 +0200

    Dynamic Values in Templates

--- React.js/moja-aplikacja/src/App.js
+++ React.js/moja-aplikacja/src/App.js
@@ -1,11 +1,25 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 60;
+  // const person = { name:'Kuba', age: 30};
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>{likes} times</p>
+
+        {/* <p>{person}</p> */}
+
+        <p>{10}</p>
+        <p>{"hello"}</p>
+        <p>{[1,2,3,4]}</p>
+        <p>{Math.random()*10}</p>
+
+        <a href={link}>Google site</a>
       </div>
     </div>
   );
