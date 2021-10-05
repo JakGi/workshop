7915082062a24de4d12f509c1f0cf495741e82f7
JakubGala <snuq321@gmail.com>
2021-10-05 13:34:37 +0200

    Multiple Components

--- React.js/moja-aplikacja/src/App.js
+++ React.js/moja-aplikacja/src/App.js
@@ -1,25 +1,15 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   const title = "Welcome to the new blog";
-  const likes = 60;
-  // const person = { name:'Kuba', age: 30};
-  const link = "http://www.google.com";
-
+  
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>{likes} times</p>
-
-        {/* <p>{person}</p> */}
-
-        <p>{10}</p>
-        <p>{"hello"}</p>
-        <p>{[1,2,3,4]}</p>
-        <p>{Math.random()*10}</p>
-
-        <a href={link}>Google site</a>
+      <Home />
       </div>
     </div>
   );
