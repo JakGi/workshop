4f5184f405e32e1e206cf9134cbb86e1ae8aa7df
JakubGala <snuq321@gmail.com>
2021-10-06 18:50:14 +0200

    Click events

--- React.js/moja-aplikacja/src/App.js
+++ React.js/moja-aplikacja/src/App.js
@@ -2,8 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog";
-  
+ 
   return (
     <div className="App">
       <Navbar />
--- React.js/moja-aplikacja/src/Home.js
+++ React.js/moja-aplikacja/src/Home.js
@@ -1,9 +1,25 @@
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
-      );
-}
- 
+  const handleClick = (e) => {
+    console.log("hello" , e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  };
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("jakub", e);
+        }}
+      >
+        Click me Again
+      </button>
+    </div>
+  );
+};
+
 export default Home;
